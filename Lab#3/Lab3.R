106f452b734112a6541064e20f36d6d5442807f4
Jan Domingo <janaarondomingo@gmail.com>
2019-04-06 12:09:16 -0700

    Appendix added to writeup. Comments added and typos fixed to Lab3.R

--- Lab#3/Lab3.R
+++ Lab#3/Lab3.R
@@ -3,8 +3,8 @@
 #1
 #(a)
 
-#This function calculates the probabilty of a "double six" showing up in 24 pair rolls for n amount of times
-probCalc = function(n) {
+#This function calculates the probability of a "double six" showing up in 24 pair rolls for n amount of times
+dieSim = function(n) {
   success = 0 #"Double six" roll counter
   for (i in 1:n) {
     rolls = replicate(24, sum(sample(1:6,2,replace=TRUE)))
@@ -15,7 +15,7 @@ probCalc = function(n) {
   return (success/n)
 }
 
-prob1000 = dieSim(1000) #Calculates the probablity of "double six" occuring in 24 die pair rolls, 1000 times
+prob1000 = dieSim(1000) #Calculates the probability of "double six" occurring in 24 die pair rolls, 1000 times
     
 
 #=========================================================================
@@ -49,10 +49,10 @@ prob50000 = bdayProb(50000)
 w = c(-10, -10, -10, 0, 0, 14)
 winnings = sample (w, 1000, replace = TRUE)  #Result of 1000 trials saved into winnings
 table (winnings) #Displays total count for -10, 0, and 14
-table (winnings)/1000 #Displays the probability distrubtion function
+table (winnings)/1000 #Displays the probability distribution function
 winProp = table(winnings)/1000 
-cumsum(winProp) #Displays the cumulative distrubtion function
-mean(winnings)
-sd(winnings)
-var(winnings)
+cumsum(winProp) #Displays the cumulative distribution function (CDF)
+mean(winnings)  #Displays sample the mean
+sd(winnings)    #Displays sample the standard deviation
+var(winnings)   #Displays sample the variance
 
