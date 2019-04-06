39bb7cba805dc9cf7158eb9a787b4823f11b8e79
Jan Domingo <janaarondomingo@gmail.com>
2019-04-05 22:09:30 -0700

    Problem 1 completed

--- Lab#3/Lab3.R
+++ Lab#3/Lab3.R
@@ -3,10 +3,17 @@
 #1
 #(a)
 
-probCalc = function(n)
-  success <- 0
-  
-  rolls <- replicate(24, sum(sample(1:6,2,replace=TRUE)))
-  if (sum(rolls == 12) > 0) ## or sum(rolls == 12) >= 1
-    success = success + 1 
-  
+#This function calculates the probabilty of a "double six" showing up in 24 pair rolls for n amount of times
+probCalc = function(n) {
+  success = 0 #"Double six" roll counter
+  for (i in 1:n) {
+    rolls = replicate(24, sum(sample(1:6,2,replace=TRUE)))
+    if (sum(rolls == 12) > 0) {
+      success = success + 1 #increment counter if at least one double six appears)
+    }
+  }
+  return (success/n)
+}
+
+prob1000 = probCalc(1000) #Calculates the probablity of "double six" occuring in 24 die pair rolls, 1000 times
+    
