f307a1f93e85a79c5e95b042511c666d0404d61c
Jan Domingo <janaarondomingo@gmail.com>
2019-05-14 19:54:08 -0700

    Final lab folder created

---
+++ FinalLab/FinalLab.R
@@ -0,0 +1,18 @@
+# Jan Domingo
+# May 16, 2019
+#Final: Lab Take-home
+
+#1
+#(a)
+
+p = 20/38 
+n = 1000 #Amount of simulations
+gamestreak = vector()
+
+for (i in 1:n) {
+  streak = 0
+  lose = 0
+  
+}
+
+
